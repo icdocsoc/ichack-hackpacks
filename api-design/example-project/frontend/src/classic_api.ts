dff6a655155291a373825e1f103b6f37eb4f6a18
Timofey Kolesnichenko <tk1124@ic.ac.uk>
2026-01-08 21:21:31 +0000

    Feat[tk1124]: Add error handling.

--- api-design/example-project/frontend/src/classic_api.ts
+++ api-design/example-project/frontend/src/classic_api.ts
@@ -2,32 +2,67 @@ const API = import.meta.env.VITE_CANONICAL_API_URL;
 
 export async function getPosts(id?: string) {
   const url = id ? `${API}/${id}` : API;
-  const res = await fetch(url);
-  return res.json();
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      const errText = await res.text();
+      throw new Error(`HTTP ${res.status}: ${errText}`);
+    }
+    return res.json();
+  } catch (err) {
+    throw new Error("Network Error");
+  }
 }
 
 export async function createPost(text: string) {
-  const res = await fetch(API, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ text }),
-  });
-  return res.json();
+  try {
+    const res = await fetch(API, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text }),
+    });
+
+    if (!res.ok) {
+      const errText = await res.text();
+      throw new Error(`HTTP ${res.status}: ${errText}`);
+    }
+    return res.json();
+  } catch (err) {
+    throw new Error("Network Error");
+  }
 }
 
 export async function updatePost(id: string, text: string) {
   const url = `${API}/${id}`;
-  const res = await fetch(url, {
-    method: "PATCH",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ text }),
-  });
-  return res.json();
+  try {
+    const res = await fetch(url, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text }),
+    });
+
+    if (!res.ok) {
+      const errText = await res.text();
+      throw new Error(`HTTP ${res.status}: ${errText}`);
+    }
+    return res.json();
+  } catch (err) {
+    throw new Error("Network Error");
+  }
 }
 
 export async function deletePost(id: string) {
   const url = `${API}/${id}`;
-  await fetch(url, {
-    method: "DELETE",
-  });
+  try {
+    const res = await fetch(url, {
+      method: "DELETE",
+    });
+
+    if (!res.ok) {
+      const errText = await res.text();
+      throw new Error(`HTTP ${res.status}: ${errText}`);
+    }
+  } catch (err) {
+    throw new Error("Network Error");
+  }
 }
--- api-design/example-project/frontend/src/packed_api.ts
+++ api-design/example-project/frontend/src/packed_api.ts
@@ -2,31 +2,70 @@ const API = import.meta.env.VITE_PACKED_API_URL;
 
 export async function getPosts() {
   const res = await fetch(API);
-  return res.json();
+
+  try {
+    if (!res.ok) {
+      const errText = await res.text();
+      throw new Error(`HTTP ${res.status}: ${errText}`);
+    }
+
+    return res.json();
+  } catch (err) {
+    throw new Error("Network Error");
+  }
 }
 
 export async function createPost(text: string) {
-  const res = await fetch(API, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ text }),
-  });
-  return res.json();
+  try {
+    const res = await fetch(API, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text }),
+    });
+
+    if (!res.ok) {
+      const errText = await res.text();
+      throw new Error(`HTTP ${res.status}: ${errText}`);
+    }
+
+    return res.json();
+  } catch (err) {
+    throw new Error("Network Error");
+  }
 }
 
 export async function updatePost(id: string, text: string) {
-  const res = await fetch(API, {
-    method: "PATCH",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ id, text }),
-  });
-  return res.json();
+  try {
+    const res = await fetch(API, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id, text }),
+    });
+
+    if (!res.ok) {
+      const errText = await res.text();
+      throw new Error(`HTTP ${res.status}: ${errText}`);
+    }
+
+    return res.json();
+  } catch (err) {
+    throw new Error("Network Error");
+  }
 }
 
 export async function deletePost(id: string) {
-  await fetch(API, {
-    method: "DELETE",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ id }),
-  });
+  try {
+    const res = await fetch(API, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id }),
+    });
+
+    if (!res.ok) {
+      const errText = await res.text();
+      throw new Error(`HTTP ${res.status}: ${errText}`);
+    }
+  } catch (err) {
+    throw new Error("Network Error");
+  }
 }
