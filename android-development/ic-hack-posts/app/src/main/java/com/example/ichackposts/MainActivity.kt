446100cf43b57bfa766542aac2bb95a83cae2717
Adam Kassam <adam.kassam23@imperial.ac.uk>
2026-01-18 16:08:46 +0000

    cleaned up MainActivity

--- android-development/ic-hack-posts/app/src/main/java/com/example/ichackposts/MainActivity.kt
+++ android-development/ic-hack-posts/app/src/main/java/com/example/ichackposts/MainActivity.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ichackposts.ui.theme.ICHackPostsTheme
 import io.ktor.client.HttpClient
@@ -158,21 +157,5 @@ fun PostCard(post: Post) {
                 color = MaterialTheme.colorScheme.outline
             )
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ICHackPostsTheme {
-        Greeting("Android")
     }
 }
